9964d9489e89bdd0ca76f7394ee5d3940d5f3905
ishizaka-kota <ishizaka-kota@aurora.vc>
2016-08-16 18:45:15 +0900

    key変更

--- keyboards/ergodox_ez/keymaps/default/keymap.c
+++ keyboards/ergodox_ez/keymaps/default/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//MDA |   @    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |CTLR  |  GUI |ALT   | ALFT | CLSFT|                                       |   [  |   ]  |      |GU/SB |   Å_   |
+ *   |CTLR  |  GUI |ALT   | ALFT | CLSFT|                                       |   [  |   ]  |  ALT |GU/SB |   Å_   |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | Home | END  |       | PgUp |PgDn    |
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(SYMB),		KC_Y,			KC_U,			KC_I,		KC_O,			KC_P,			JA_ENVL,
              				KC_H,			KC_J,			KC_K,		KC_L,			KC_SCLN,		JA_CLON,
              MEH_T(KC_NO),	KC_N,			KC_M,			KC_COMM,	KC_DOT,			LT(MDIA, KC_SLSH),JA_AT,
-             								JA_LBRC,		JA_RBRC,	KC_RALT,		LT(MDIA, SYMB),	JA_ENUN,
+             								JA_LBRC,		JA_RBRC,	KC_RALT,		LT(SYMB, KC_LGUI),	JA_ENUN,
              KC_PGUP,		KC_PGDN,
              KC_UP,
              KC_DOWN,		KC_ENT,			KC_TAB
