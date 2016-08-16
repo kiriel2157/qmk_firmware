7abb571f623bcab3476d4a7616dc1fd6b1d4e855
ishizaka-kota <ishizaka-kota@aurora.vc>
2016-08-16 16:35:14 +0900

    key変更

--- keyboards/ergodox_ez/keymaps/default/keymap.c
+++ keyboards/ergodox_ez/keymaps/default/keymap.c
@@ -45,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
              KC_6,			KC_7,			KC_8,			KC_9,		KC_0,			KC_MINS,		KC_EQL,
              TG(SYMB),		KC_Y,			KC_U,			KC_I,		KC_O,			KC_P,			KC_JYEN,
-             KC_H,			KC_J,			KC_K,			KC_L,		KC_SCLN,		KC_QUOT,
+             				KC_H,			KC_J,			KC_K,		KC_L,			KC_SCLN,		KC_QUOT,
              MEH_T(KC_NO),	KC_N,			KC_M,			KC_COMM,	KC_DOT,			LT(MDIA, KC_SLSH),KC_RSFT,
-             KC_LBRC,		KC_RBRC,		KC_RALT,			KC_FN1,			KC_RCTRL
+             								KC_LBRC,		KC_RBRC,	KC_RALT,		KC_FN1,			KC_RCTRL,
              KC_PGUP,		KC_PGDN,
              KC_UP,
              KC_DOWN,		KC_ENT,			KC_TAB
